660c0b2364e35cef528685ad5cdf61ae7242b4de
Ascendus <ascendus.dev@gmail.com>
2022-07-12 13:08:40 +1200

    feat: added better logging

--- src/index.ts
+++ src/index.ts
@@ -8,5 +8,11 @@ fetch(ENDPOINT)
     .then((res: APIResponse): void => {
         res.result = res.result.replace(EXCESSIVE_TEXT, "");
         writeFileSync("output.txt", res.result);
-        console.log(res.result);
+        console.log(`[process] Completed GET request, view the response in ${process.cwd()}\\output.txt (hold ctrl and click on the file text)`);
     })
+    .catch((error: Error): void => {
+        console.log(`[process] ${error.stack}`);
+        process.exit(1);
+    });
+
+process.on("exit", (code: number): void => console.log(`[process] exiting process with code ${code}`));
